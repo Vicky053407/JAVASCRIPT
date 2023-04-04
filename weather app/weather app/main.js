ef951865236e8554e2789a51a9b0ffd9e567c433
Vicky03407 <vicky053407@gmail.com>
2023-04-04 22:46:18 +0530

    'okay'

--- weather app/weather app/main.js
+++ weather app/weather app/main.js
@@ -14,7 +14,7 @@ var wind = document.getElementById('wind');
 var pres = document.getElementById('pres');
 var hum = document.getElementById('hum');
 var img = document.getElementById('img');
-
+  
 
 
 
@@ -22,13 +22,13 @@ function getCurrent(){
 
     navigator.geolocation.getCurrentPosition(async (result)=>{
         const l = result.coords;
-        lon = l.longitude;
+        lon = l.longitude; 
         lat = l.latitude;
         console.log({"lon":lon, "lat":lat});
         const data = await fetch(`${BASE_URL}weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
         console.log(data);
         if(data.status==200){
-
+ 
             const info = await data.json()
             console.log(info);
             name.innerhtml = info.name;
@@ -53,7 +53,7 @@ async function search(){
     name.innerhtml = info.name;
     temp.innerHTML = info.main.temp;
     wind.innerHTML = info.wind.speed;
-    img.src=https//openweathermap.org/img/wn/${info.weather[0].icon}@2x.png;
+    img.src=https;//openweathermap.org/img/wn/${info.weather[0].icon}@2x.png;
     pres.innerHTML = info.main.pressure;
     hum.innerHTML = info.main.humidity;
 
